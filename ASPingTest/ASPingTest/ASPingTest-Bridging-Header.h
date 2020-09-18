567cfdd4d42391f566f3343c8af582f92d423d06
Avadhesh Sharma <apple.avadhesh@gmail.com>
2020-09-18 13:27:37 +0530

    SimplePing Delegate Added
    SimplePing Delegate Added, Error received when internet unavailable

--- ASPingTest/ASPingTest/ASPingTest-Bridging-Header.h
+++ ASPingTest/ASPingTest/ASPingTest-Bridging-Header.h
@@ -3,3 +3,4 @@
 //
 
 #include "SimplePing.h"
+#include "LDSRouterInfo.h"
--- ASPingTest/ASPingTest/ViewController.swift
+++ ASPingTest/ASPingTest/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SimplePingDelegate {
+    
+    //MARK:
+    var pinger: SimplePing? = nil
+    var timer: DispatchSourceTimer? = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // initial setup
+        getRouterInfo()
     }
-
-
+    
+    fileprivate func getRouterInfo() {
+        let router = LDSRouterInfo.getRouterInfo()
+        pinger = SimplePing(hostName: router["ip"] as! String)
+        pinger?.delegate = self
+        pinger?.start()
+    }
+    
+    
+    //MARK: SimplePing Delegates
+    func simplePing(_ pinger: SimplePing, didStartWithAddress address: Data) {
+        if (timer != nil) {
+            return
+        }
+        timer = DispatchSource.makeTimerSource(queue: DispatchQueue.main)
+        timer?.schedule(deadline: .now(), repeating: .seconds(1), leeway: .seconds(1))
+        timer?.setEventHandler(handler: {
+            pinger.send(with: nil)
+        })
+        timer?.resume()
+    }
+    
+    func simplePing(_ pinger: SimplePing, didSendPacket packet: Data, sequenceNumber: UInt16) {
+        print("PING: success!!!")
+    }
+    
+    func simplePing(_ pinger: SimplePing, didReceivePingResponsePacket packet: Data, sequenceNumber: UInt16) {
+        print("PING: received \(packet)")
+    }
+    
+    func simplePing(_ pinger: SimplePing, didFailToSendPacket packet: Data, sequenceNumber: UInt16, error: Error) {
+        let err = error as NSError
+        if err.code == 65 {
+            print("PING: transmit failed!!!")
+        }
+    }
 }
 
