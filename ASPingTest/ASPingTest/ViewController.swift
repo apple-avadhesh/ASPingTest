f445b7c06417e7d90b1fdd8e539e5c867fdde338
Avadhesh Sharma <apple.avadhesh@gmail.com>
2020-09-18 12:18:22 +0530

    Intial Project Creation
    Created Project

---
+++ ASPingTest/ASPingTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ASPingTest
+//
+//  Created by PC Gamer on 18/09/20.
+//  Copyright © 2020 Avadhesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
